53e1c0494ac1723a84730e073061914704c705d5
Matzo <ksk.matsuo@gmail.com>
2015-01-22 08:31:04 +0900

    first commit

---
+++ FrameworkTest/Classes/Sample.swift
@@ -0,0 +1,15 @@
+//
+//  Sample.swift
+//  FrameworkTest
+//
+//  Created by Matsuo Keisuke on 1/22/15.
+//  Copyright (c) 2015 Matsuo Keisuke. All rights reserved.
+//
+
+import Foundation
+
+class Sample {
+    class func say() {
+        NSLog("use \(object_getClassName(Sample))");
+    }
+}
---
+++ Samply/Samply.h
@@ -0,0 +1,19 @@
+//
+//  Samply.h
+//  Samply
+//
+//  Created by Matsuo Keisuke on 1/22/15.
+//  Copyright (c) 2015 Matsuo Keisuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Samply.
+FOUNDATION_EXPORT double SamplyVersionNumber;
+
+//! Project version string for Samply.
+FOUNDATION_EXPORT const unsigned char SamplyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Samply/PublicHeader.h>
+
+
